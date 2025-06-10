b62f773e678b7f3086a0c9fcfa8b7ed31cb961ac
asouzale <leeh.souza16@gmail.com>
2025-06-09 21:50:34 -0300

    aula 0906

--- Greenfoot/Aula 01/MundoAbelha/Abelha.java
+++ Greenfoot/Aula 01/MundoAbelha/Abelha.java
@@ -8,6 +8,28 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Abelha extends Actor
 {
+    //Definindo os fields
+    int vidas;
+    int score;
+    int PONTOS = 100;
+    int indice;
+    GreenfootImage imgs [];
+    //Definindo o constructor
+    /**
+     * Constructor da Classe Abelha
+     */
+    public Abelha(){
+        vidas = 3; //vai ter 3 vidas
+        score = 0;
+        //GreenfootImage img = new GreenfootImage ("bee01.png");
+        //setImage (img);
+        indice=0;
+        imgs = new GreenfootImage [4]; //Definindo vetor 4 posições
+        for (int i=0; i<4 ; i++){
+            imgs[i] = new GreenfootImage ("bee0" + (i+1) + ".png");
+        }
+        setImage(imgs[indice]);
+    }
     /**
      * Act - do whatever the Abelha wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -15,32 +37,37 @@ public class Abelha extends Actor
     public void act()
     {
         // Add your action code here.
-        move(1);
-        verificarPosição();
+        move(3);
         if (Greenfoot.isKeyDown("left")){
             turn(-5);
         }
         if (Greenfoot.isKeyDown("right")){
-            turn(5);
+            turn(5);  
         }
-    }
-    
-    public void verificarPosição(){
-      if (estaNoTopo()){
-          setLocation(getX(), getWorld().getHeight()-10);
-      }
-      if (estaNaBase()){
-          setLocation(getX(), 10);
-      }
-      if (estaNaDireita()){
-          setLocation(10, getY());
-      }
-      if (estaNaEsquerda()){
-          setLocation(getWorld().getWidth()-10, getY());        
-      }
+        verificarPosicao();
+        capturarMosca();
+        serCapturadoPelaAranha();
+        animarAbelha();
     }
     /**
-     * Método que verifica se a Abelha está no topo
+     * Método que verifica posição da Abelha.
+     */
+    public void verificarPosicao(){
+        if (estaNoTopo()){
+            setLocation(getX(), getWorld().getHeight()-10);
+        }
+        if (estaNaBase()){
+            setLocation(getX(), 10);
+        }
+        if (estaNaDireita()){
+            setLocation(10, getY());
+        }
+        if (estaNaEsquerda()){
+            setLocation(getWorld().getWidth()-10, getY());
+        }
+    }
+    /**
+     * Método que verifica se a abelha está no topo.
      */
     public boolean estaNoTopo(){
         if (getY()<10){
@@ -50,21 +77,78 @@ public class Abelha extends Actor
         }
     }
     /**
-     * Método que verifica se a Abelha está na base
+     * Método que verifica se abelha está na base
      */
     public boolean estaNaBase(){
         return getY()>getWorld().getHeight()-10;
     }
     /**
-     * Método que verifica se a Abelha está na esquerda
-     */
+     * Método que verifica se abelha está na esquerda
+     */    
     public boolean estaNaEsquerda(){
         return getX()<10;
     }
-    /** 
-     * Método que verifica se a Abelha está na dreita
+    /**
+     * Método que verifica se abelha está na direita.
      */
     public boolean estaNaDireita(){
         return getX()>getWorld().getWidth()-10;
+    }
+    
+    public void capturarMosca(){
+        if (isTouching(Mosca.class)) {
+            removeTouching(Mosca.class);
+            Greenfoot.playSound("slurp.wav");
+            atualizarScore();
+            //Vai de 1 a 800
+            int posX = Greenfoot.getRandomNumber(
+                        getWorld().getWidth()) + 1;
+            //Vai de 1 a 600
+            int posY = Greenfoot.getRandomNumber(
+                        getWorld().getHeight()) + 1;
+            //Criando a mosca
+            Mosca mosca = new Mosca(Greenfoot.getRandomNumber(3) + 1, 
+            Greenfoot.getRandomNumber(360) );
+            
+            //Colocando no mundo na posição X, Y
+            getWorld().addObject(mosca, posX, posY);
+        }
+    }
+    
+    public void capturarMosca2(){
+        Actor mosca = getOneIntersectingObject(Mosca.class);
+        if (mosca != null){
+            getWorld().removeObject(mosca);
+        }
+    }
+    
+    public void serCapturadoPelaAranha(){
+        if (isTouching(Aranha.class)){
+            //Vai de 1 a 800
+            int posX = Greenfoot.getRandomNumber(
+                        getWorld().getWidth()) + 1;
+            //Vai de 1 a 600
+            int posY = Greenfoot.getRandomNumber(
+                        getWorld().getHeight()) + 1;
+            setLocation(posX, posY);
+            Greenfoot.playSound("ouch.wav");
+            vidas--;//vidas = vidas - 1
+            if (vidas<=0){
+                getWorld().showText("GAME OVER", 400, 300);
+                Greenfoot.stop();
+            }
+        }
+    }
+    
+    public void atualizarScore(){
+        score += PONTOS; //score = score + PONTOS
+        getWorld().showText("Score: " + score, 100, 10);
+    }
+    
+    public void animarAbelha (){
+        // % é o resto da divisão (ex 10/3 o resto é 1)
+        indice = (indice + 1) % 4;
+        setImage (imgs[indice]);
+        
     }
 }
--- Greenfoot/Aula 01/MundoAbelha/BeeWorld.java
+++ Greenfoot/Aula 01/MundoAbelha/BeeWorld.java
@@ -28,32 +28,13 @@ public class BeeWorld extends World
     {
         Aranha aranha = new Aranha();
         addObject(aranha,91,332);
+        
         Abelha abelha = new Abelha();
         addObject(abelha,77,150);
-        Mosca mosca = new Mosca();
-        addObject(mosca,484,171);
-        Mosca mosca2 = new Mosca();
-        addObject(mosca2,454,299);
-        Mosca mosca3 = new Mosca();
-        addObject(mosca3,268,83);
-        Mosca mosca4 = new Mosca();
-        addObject(mosca4,257,364);
-        Mosca mosca5 = new Mosca();
-        addObject(mosca5,584,15);
-        mosca.setLocation(492,362);
-        mosca2.setLocation(548,307);
-        mosca4.setLocation(480,296);
-        mosca3.setLocation(414,320);
-        mosca5.setLocation(419,382);
-        mosca3.setLocation(83,454);
-        mosca5.setLocation(72,501);
-        mosca.setLocation(75,548);
-        mosca4.setLocation(120,487);
-        mosca2.setLocation(120,539);
-        abelha.setLocation(84,152);
-        mosca4.setLocation(662,384);
-        mosca3.setLocation(494,114);
-        mosca5.setLocation(275,366);
-        mosca2.setLocation(507,521);
+        
+        for (int qtd=0;qtd<20; qtd++){
+            int posX = Greenfoot.getRandomNumber(800); 
+            int posY = Greenfoot.getRandomNumber(600);
+        }
     }
 }
